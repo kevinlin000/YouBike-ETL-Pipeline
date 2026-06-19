623d1c1b8b09e58f98fccd893adbf123d598045b
kevinlin000 <guguji1444@gmail.com>
2026-06-19 22:42:25 +0800

    Fix district peak-hour mart grouping

--- analytics/dbt/models/marts/mart_district_peak_hour_health.sql
+++ analytics/dbt/models/marts/mart_district_peak_hour_health.sql
@@ -1,31 +1,41 @@
+with station_hourly as (
+    select
+        s.station_no,
+        i.district,
+        date_format(s.record_time, '%Y-%m-%d %H:00:00') as record_hour,
+        hour(s.record_time) as hour_of_day,
+        case
+            when hour(s.record_time) between 7 and 9
+                or hour(s.record_time) between 17 and 19
+                then 'peak'
+            else 'off_peak'
+        end as period_type,
+        s.bikes_available,
+        s.spaces_available,
+        i.total_spaces,
+        s.is_stock_out_risk,
+        s.is_full_load_risk
+    from {{ ref('stg_station_status') }} as s
+    inner join {{ ref('stg_station_info') }} as i
+        on s.station_no = i.station_no
+)
+
 select
-    concat(i.district, '|', date_format(s.record_time, '%Y-%m-%d %H:00:00')) as district_hour_key,
-    i.district,
-    date_format(s.record_time, '%Y-%m-%d %H:00:00') as record_hour,
-    hour(s.record_time) as hour_of_day,
-    case
-        when hour(s.record_time) between 7 and 9
-            or hour(s.record_time) between 17 and 19
-            then 'peak'
-        else 'off_peak'
-    end as period_type,
-    count(distinct s.station_no) as stations_observed,
+    concat(district, '|', record_hour) as district_hour_key,
+    district,
+    record_hour,
+    hour_of_day,
+    period_type,
+    count(distinct station_no) as stations_observed,
     count(*) as observations,
-    avg(s.bikes_available) as avg_bikes_available,
-    avg(s.spaces_available) as avg_spaces_available,
-    avg(s.bikes_available / nullif(i.total_spaces, 0)) as avg_filling_rate,
-    sum(s.is_stock_out_risk) as stock_out_risk_observations,
-    sum(s.is_full_load_risk) as full_load_risk_observations
-from {{ ref('stg_station_status') }} as s
-inner join {{ ref('stg_station_info') }} as i
-    on s.station_no = i.station_no
+    avg(bikes_available) as avg_bikes_available,
+    avg(spaces_available) as avg_spaces_available,
+    avg(bikes_available / nullif(total_spaces, 0)) as avg_filling_rate,
+    sum(is_stock_out_risk) as stock_out_risk_observations,
+    sum(is_full_load_risk) as full_load_risk_observations
+from station_hourly
 group by
-    i.district,
-    date_format(s.record_time, '%Y-%m-%d %H:00:00'),
-    hour(s.record_time),
-    case
-        when hour(s.record_time) between 7 and 9
-            or hour(s.record_time) between 17 and 19
-            then 'peak'
-        else 'off_peak'
-    end
+    district,
+    record_hour,
+    hour_of_day,
+    period_type
